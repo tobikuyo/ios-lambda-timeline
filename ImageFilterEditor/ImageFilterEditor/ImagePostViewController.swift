b6864d395143dbe9871105ee98b9e2b4ffdb8325
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-04 23:06:46 +0100

    Method to ApplyFilter to Images Setup

--- ImageFilterEditor/ImageFilterEditor/ImagePostViewController.swift
+++ ImageFilterEditor/ImageFilterEditor/ImagePostViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Photos
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 class ImagePostViewController: UIViewController {
 
@@ -14,13 +17,45 @@ class ImagePostViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var changeFilterButton: UIButton!
+    @IBOutlet weak var savePhotoButton: UIButton!
+
+    // MARK: - Properties
+
+    var originalImage: UIImage?
+
+    private var motionBlur = CIFilter(name: "CIMotionBlur")
+    private var sepiaTone = CIFilter(name: "CIPhotoEffectProcess")
+    private var tonal = CIFilter(name: "CIPhotoEffectTonal")
+    private var mono = CIFilter(name: "CIPhotoEffectMono")
+    private var transfer = CIFilter(name: "CIPhotoEffectTransfer")
+
+    private var context = CIContext(options: nil)
 
     // MARK: - View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        changeFilterButton.layer.cornerRadius = 4
+        savePhotoButton.layer.cornerRadius = 4
+    }
 
-        // Do any additional setup after loading the view.
+    // MARK: - Actions
+
+    func applyFilter(to image: UIImage, with filter: CIFilter) -> UIImage? {
+        guard let cgImage = image.cgImage else { return nil }
+        let ciImage = CIImage(cgImage: cgImage)
+
+        filter.setValue(ciImage, forKey: kCIInputImageKey)
+        filter.setValue(slider.value, forKey: kCIInputIntensityKey)
+
+        guard
+            let outputCIImage = filter.outputImage,
+            let outputCGImage = context.createCGImage(outputCIImage,
+                                                      from: CGRect(origin: .zero, size: image.size)) else { return nil }
+
+        return UIImage(cgImage: outputCGImage)
     }
 
     // MARK: - IBActions
