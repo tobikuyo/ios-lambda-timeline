b50e2a9ad850db1ceea90af018ef5684b3778832
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-06 16:40:47 +0100

    Recording Model Created

---
+++ AudioComments/AudioComments/Recording.swift
@@ -0,0 +1,16 @@
+//
+//  Recording.swift
+//  AudioComments
+//
+//  Created by Tobi Kuyoro on 06/05/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import Foundation
+
+struct Recording {
+    var title: String
+    var fileURL: URL
+    var duration: TimeInterval
+    var currentTime: TimeInterval
+}
