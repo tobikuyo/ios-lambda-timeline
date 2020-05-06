1a5a6584af167efc91b6a24a4bf68198427d9032
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-06 23:11:17 +0100

    Base Setup Based on Guided Project

---
+++ VideoPosts/VideoPosts/CameraViewController.swift
@@ -0,0 +1,141 @@
+//
+//  CameraViewController.swift
+//  VideoPosts
+//
+//  Created by Tobi Kuyoro on 06/05/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraViewController: UIViewController {
+
+    // MARK: - Outlets
+
+    @IBOutlet var recordButton: UIButton!
+    @IBOutlet var cameraView: CameraPreviewView!
+
+    // MARK: - Properties
+
+    lazy private var captureSession = AVCaptureSession()
+    lazy private var fileOutput  = AVCaptureMovieFileOutput()
+
+    private var player: AVPlayer!
+
+    // MARK: - View Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Resize camera preview to fill the entire screen
+        cameraView.videoPlayerView.videoGravity = .resizeAspectFill
+        setUpCaptureSession()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        captureSession.startRunning()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        captureSession.stopRunning()
+    }
+
+    // MARK: - IBOutlets
+
+    @IBAction func recordButtonPressed(_ sender: Any) {
+        toggleRecord()
+    }
+    
+    // MARK: - Actions
+
+    private func updateViews() {
+        recordButton.isSelected = fileOutput.isRecording
+    }
+
+    private func setUpCaptureSession() {
+        captureSession.beginConfiguration()
+        let camera = bestCamera()
+
+        guard let cameraInput = try? AVCaptureDeviceInput(device: camera),
+            captureSession.canAddInput(cameraInput) else {
+                fatalError("Error adding camera to capture session")
+        }
+
+        captureSession.addInput(cameraInput)
+
+        if captureSession.canSetSessionPreset(.hd1920x1080) {
+            captureSession.sessionPreset = .hd1920x1080
+        }
+
+        guard captureSession.canAddOutput(fileOutput) else {
+            fatalError("Error: Cannot save video with capture session")
+        }
+        captureSession.addOutput(fileOutput)
+        captureSession.commitConfiguration()
+        cameraView.session = captureSession
+    }
+
+    private func bestCamera() -> AVCaptureDevice {
+        if let ultraWideCamera = AVCaptureDevice.default(.builtInUltraWideCamera, for: .video, position: .back) {
+            return ultraWideCamera
+        }
+
+        if let wideAngleCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+            return wideAngleCamera
+        }
+
+        fatalError("No camera available - are you on a simulator?")
+    }
+
+    private func toggleRecord() {
+        if fileOutput.isRecording {
+            fileOutput.stopRecording()
+        } else {
+            fileOutput.startRecording(to: newRecordingURL(), recordingDelegate: self)
+        }
+    }
+
+    private func newRecordingURL() -> URL {
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = [.withInternetDateTime]
+
+        let name = formatter.string(from: Date())
+        let fileURL = documentsDirectory.appendingPathComponent(name).appendingPathExtension("mov")
+        return fileURL
+    }
+
+    private func playMovie(url: URL) {
+        player = AVPlayer(url: url)
+        let playerView = VideoPlayerView()
+        playerView.player = player
+        var topRect = view.bounds
+        topRect.size.height = topRect.size.height / 4
+        topRect.size.width = topRect.size.width / 4
+        topRect.origin.y = view.layoutMargins.top
+        playerView.frame = topRect
+        view.addSubview(playerView)
+        player.play()
+    }
+}
+
+extension CameraViewController: AVCaptureFileOutputRecordingDelegate {
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        if let error = error {
+            print("Error saving video: \(error)")
+        } else {
+            playMovie(url: outputFileURL)
+        }
+
+        updateViews()
+    }
+
+    func fileOutput(_ output: AVCaptureFileOutput, didStartRecordingTo fileURL: URL, from connections: [AVCaptureConnection]) {
+        print("Started recording: \(fileURL)")
+        updateViews()
+    }
+}
