03eb2b7876253912a2374732f4e486570daff380
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-07 00:13:51 +0100

    Created a Recording Model

---
+++ VideoPosts/VideoPosts/Recording.swift
@@ -0,0 +1,14 @@
+//
+//  Recording.swift
+//  VideoPosts
+//
+//  Created by Tobi Kuyoro on 07/05/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import Foundation
+
+struct Recording {
+    var title: String
+    var url: URL
+}
