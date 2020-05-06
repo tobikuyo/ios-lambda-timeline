a040d3612ce6ff99b9a9f33b56e623214f26d2a3
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-06 22:50:28 +0100

    CameraPreviewView Added

---
+++ VideoPosts/VideoPosts/CameraPreviewView.swift
@@ -0,0 +1,26 @@
+//
+//  CameraPreviewView.swift
+//  VideoPosts
+//
+//  Created by Tobi Kuyoro on 06/05/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraPreviewView: UIView {
+
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+
+    var videoPlayerView: AVCaptureVideoPreviewLayer {
+        return layer as! AVCaptureVideoPreviewLayer
+    }
+
+    var session: AVCaptureSession? {
+        get { return videoPlayerView.session }
+        set { videoPlayerView.session = newValue }
+    }
+}
